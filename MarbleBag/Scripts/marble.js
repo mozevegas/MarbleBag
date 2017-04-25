2f53619a34527d053310ac57a1a56ba282856c25
moevegas <moevegas@gmail.com>
2017-04-25 14:28:23 -0400

    Basics working

---
+++ MarbleBag/Scripts/marble.js
@@ -0,0 +1,21 @@
+﻿let MarblePicker = () => {
+    $();
+}
+
+let MarbleCreator = () => {
+    let _NewColor = $("#ColorNew").val();
+    $("#NewColor").html(_NewColor);
+}
+
+let TalkServer = () => {
+    $.ajax({
+        url: '/api/pick',
+        dataType: "json",
+        success: (data) => {
+            console.log("yes", data);
+        },
+        error: (data) => {
+            console.log("hell no", data);
+        },
+    });
+}
